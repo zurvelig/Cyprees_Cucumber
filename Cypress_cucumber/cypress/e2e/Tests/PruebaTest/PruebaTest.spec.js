a06e34c21abe8beb05654fb016e83ea5af25fd48
zurvelig <zurvelig@gmail.com>
2022-11-08 16:45:07 -0400

    Update PruebaTest.spec.js

--- Cypress_cucumber/cypress/e2e/Tests/PruebaTest/PruebaTest.spec.js
+++ Cypress_cucumber/cypress/e2e/Tests/PruebaTest/PruebaTest.spec.js
@@ -3,20 +3,22 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 
 Given("I navigate to the Website", () => {
-    cy.visit("http://automationpractice.com/");
+    cy.visit("https://practicetestautomation.com/practice-test-login/");
     cy.wait(10000);
     //chromeWebSecurity: false;
 });
 When("I enter user name and password and clic", () => {
    // cy.contains("Sing in").click();
-    cy.contains("Sign in").click();
-    cy.get("#email").clear();
-    cy.get("#email").type("qatube@yopmail.com");
     
-    cy.get("#passwd").clear();
-    cy.get("#passwd").type("12345");
-    cy.get("#SubmitLogin").click();
+    cy.contains("username").click();
+    cy.get("#username").clear();
+    cy.get("#username").type("student");
+    
+    cy.get("#password").clear();
+    cy.get("#password").type("Password123");
+    cy.contains("Submit").click();
 });
 Then("Validate the title after login", () => {
-    cy.title().should('eq', 'My account - My Store');
+    cy.title().should('eq', 'Logged In Successfully | Practice Test Automation');
+    cy
 });
